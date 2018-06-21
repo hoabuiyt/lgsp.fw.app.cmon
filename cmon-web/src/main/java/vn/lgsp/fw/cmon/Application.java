524da942214c539178d65a42365d6cfffb687e9b
Trandtb <trandtb@greenglobal.vn>
2018-06-21 11:07:40 +0700

    add pac4j-saml

--- cmon-web/src/main/java/vn/lgsp/fw/cmon/Application.java
+++ cmon-web/src/main/java/vn/lgsp/fw/cmon/Application.java
@@ -1,5 +1,12 @@
 package vn.lgsp.fw.cmon;
 
+import java.util.Arrays;
+
+import org.pac4j.core.client.Clients;
+import org.pac4j.core.config.Config;
+import org.pac4j.saml.client.SAML2Client;
+import org.pac4j.saml.client.SAML2ClientConfiguration;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +14,7 @@ import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -19,18 +27,34 @@ import vn.lgsp.fw.cmon.domain.AuditorAwareImpl;
 import vn.lgsp.fw.cmon.web.CacheFilter;
 import vn.lgsp.fw.core.BaseRepositoryImpl;
 
-//@ComponentScan(basePackages={"vn.lgsp.fw"})
 @EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl.class)
 @EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
-@EnableWebSecurity
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
-
+	
+	private String keystorePath = "resource:wso2carbon.jks";
+	private String keystorePassword = "wso2carbon";
+	private String privateKeyPassword = "wso2carbon";
+	private String identityProviderMetadataPath = "resource:is-metadata.xml";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+			System.out.println(":::::" + beanNames.length + " beans");
+		};
+	}
+		
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
@@ -38,16 +62,33 @@ public class Application extends SpringBootServletInitializer{
 	
 	@Bean
     public AuditorAware<String> auditorAware() {
+		System.out.println("auditorAware");
         return new AuditorAwareImpl();
     }
 	
 	@Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		System.out.println("propertySourcesPlaceholderConfigurer");
         return new PropertySourcesPlaceholderConfigurer();
     }
 
-	//@Autowired
-	//CmonDanTocRestController cmonDanTocRestController;
+	@Bean
+    public Config samlconfig() {
+		System.out.println("SpringSecurityConfig config");
+		final SAML2ClientConfiguration samlCfg = new SAML2ClientConfiguration();
+		samlCfg.setKeystorePath(keystorePath);
+		samlCfg.setKeystorePassword(keystorePassword);
+		samlCfg.setPrivateKeyPassword(privateKeyPassword);	
+		samlCfg.setIdentityProviderMetadataPath(identityProviderMetadataPath);	
+		samlCfg.setMaximumAuthenticationLifetime(3600);
+		samlCfg.setServiceProviderEntityId("cmon_app");
+		samlCfg.setServiceProviderMetadataPath("sp-metadata.xml");
+	    final SAML2Client saml2Client = new SAML2Client(samlCfg);
+	    final Clients clients = new Clients("http://localhost:8082/cmon-web/callback", saml2Client);
+	    final Config samlConfig = new Config(clients);
+		return samlConfig;
+	}
+	
 	
 	//@Bean
 	public FilterRegistrationBean cacheFilter() {
--- cmon-web/src/main/java/vn/lgsp/fw/cmon/config/SpringSecurityConfig.java
+++ cmon-web/src/main/java/vn/lgsp/fw/cmon/config/SpringSecurityConfig.java
@@ -1,16 +1,31 @@
 package vn.lgsp.fw.cmon.config;
 
+import org.pac4j.core.client.Clients;
+import org.pac4j.core.config.Config;
+import org.pac4j.saml.client.SAML2Client;
+import org.pac4j.saml.client.SAML2ClientConfiguration;
+import org.pac4j.springframework.security.web.LogoutFilter;
+import org.pac4j.springframework.security.web.SecurityFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import vn.lgsp.fw.cmon.web.AppAuthenticationSuccessHandler;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+    Config config;
+	
 	@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -36,9 +51,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	    web.ignoring().antMatchers("/api/**");
 	}
 	
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        /*http
         	.csrf().disable()
           .authorizeRequests()//.anyRequest().permitAll();
           .antMatchers("/login*","/zkau/**").permitAll()
@@ -50,7 +66,21 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
           .passwordParameter("password")
           .successHandler(new AppAuthenticationSuccessHandler())
           .and()
-          .logout().logoutSuccessUrl("/login");
+          .logout().logoutSuccessUrl("/login");*/
+    	System.out.println("securityyyyyyyyyyyyyyyy configure");
+    	 final SecurityFilter filterLogin = new SecurityFilter(config, "Saml2Client");
+         final LogoutFilter filterLogout = new LogoutFilter(config, "/");
+         filterLogout.setDestroySession(true);
+
+         http
+                 .antMatcher("/**")
+                 .authorizeRequests()
+                     .antMatchers("/","/logout").permitAll()
+                     .antMatchers("/**").authenticated()
+                 .and()
+                 .addFilterBefore(filterLogin, BasicAuthenticationFilter.class)
+                 .addFilterBefore(filterLogout, BasicAuthenticationFilter.class)
+                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
     }
     
 }
